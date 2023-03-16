8eb132a9f225ad56c4dbcf3dcd0f580be6c88b4a
Kelsey Roy <kroy@8thlight.com>
2023-03-16 08:59:52 -0700

    initialized first test and initialized the board matrix

--- conwayGameOfLife.js
+++ conwayGameOfLife.js
@@ -1,4 +1,20 @@
-const conwayGameOfLife = () => {
+const conwayGameOfLife = (rows, columns) => {
+    let board = new Array(rows);
+    for (let i = 0; i < rows; i++) {
+        board[i] = new Array(columns).fill(0)
+    }
+    console.log(typeof(board))
+    return board;
+
+    // [
+    //     0, 0, 0, 0,
+    //     0, 0, 1, 0,
+    //     0, 1, 1, 0,
+    //     0, 0, 0, 0,
+    // ]
+
+    // cellObj = {}
+
 
 }
 
--- conwayGameOfLife.test.js
+++ conwayGameOfLife.test.js
@@ -1,2 +1,10 @@
 const conwayGameOfLife = require("./conwayGameOfLife");
 
+const grid = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+test("returns a grid or matrix to represent board", () => {
+    expect(conwayGameOfLife(4, 4)).toBe(grid);
+})
+// test("returns a grid or matrix to represent board", () => {
+//     expect(conwayGameOfLife(4, 4)).toBe(grid);
+// })
