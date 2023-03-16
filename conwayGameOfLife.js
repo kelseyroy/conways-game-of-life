656a418dec85970696f03050163f49b66b07f9ce
Kelsey Roy <kroy@8thlight.com>
2023-03-16 09:56:33 -0700

    added test to check if cells are updated on grid

--- conwayGameOfLife.js
+++ conwayGameOfLife.js
@@ -1,21 +1,25 @@
-const conwayGameOfLife = (rows, columns) => {
+const conwayGameOfLife = (rows, columns, cells) => {
     let board = new Array(rows);
     for (let i = 0; i < rows; i++) {
         board[i] = new Array(columns).fill(0)
     }
-    console.log(typeof(board))
+
+    if (cells) {
+        cells.forEach(cell => {
+            let x = cell[0];
+            let y = cell[1];
+            board[x][y] = 1;
+        })
+    }
+
+    for (let i = 0; i < rows; i++) {
+        for (let i = 0; i < rows; i++) {
+        board[i] = 
+    }
+
     return board;
-
-    // [
-    //     0, 0, 0, 0,
-    //     0, 0, 1, 0,
-    //     0, 1, 1, 0,
-    //     0, 0, 0, 0,
-    // ]
-
-    // cellObj = {}
-
 
 }
 
+// conwayGameOfLife(4,4, [[1,2], [2, 1], [2, 2]])
 module.exports = conwayGameOfLife;
--- conwayGameOfLife.test.js
+++ conwayGameOfLife.test.js
@@ -1,10 +1,20 @@
 const conwayGameOfLife = require("./conwayGameOfLife");
 
-const grid = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+let grid = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
 test("returns a grid or matrix to represent board", () => {
-    expect(conwayGameOfLife(4, 4)).toBe(grid);
+    expect(conwayGameOfLife(4, 4)).toEqual(grid);
 })
-// test("returns a grid or matrix to represent board", () => {
-//     expect(conwayGameOfLife(4, 4)).toBe(grid);
-// })
+
+let updatedGrid = [[0,0,0,0],[0,0,1,0],[0,1,1,0],[0,0,0,0]]
+let cells = [[1,2], [2, 1], [2, 2]]
+
+test("returns an updated grid or matrix", () => {
+    expect(conwayGameOfLife(4, 4, cells)).toEqual(updatedGrid);
+})
+
+let unsustainableCells = [[0,0], [3,3]];
+
+test("return an empty board if the cells have less than 2 live neighbors", () => {
+    expect(conwayGameOfLife(4, 4, cells)).toEqual(grid);
+})
